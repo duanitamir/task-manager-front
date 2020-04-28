8f4274cf0b08e7313168618dea30fde9a61f4996
tamir <duani@campus.tu-berlin.de>
2020-04-28 17:06:03 +0200

    changed build

---
+++ build/precache-manifest.be783c5fb44be825efca205ea2347b0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50647a04236f4422f8ceb123dd5f429",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25acd065bcb0fd8d58e2",
+    "url": "/static/css/main.87dbca51.chunk.css"
+  },
+  {
+    "revision": "9d4904e2e55e6548d738",
+    "url": "/static/js/2.95f62ed5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.95f62ed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25acd065bcb0fd8d58e2",
+    "url": "/static/js/main.c4141e40.chunk.js"
+  },
+  {
+    "revision": "af85adea6488805b945b",
+    "url": "/static/js/runtime-main.29ea499e.js"
+  }
+]);
